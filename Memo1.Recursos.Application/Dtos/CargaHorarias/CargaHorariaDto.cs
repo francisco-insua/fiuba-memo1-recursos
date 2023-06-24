37f024c97c3878dc0a5f436ecb81a773a32ac247
Alan Cristian Goyzueta <alanlata@gmail.com>
2023-06-24 13:04:16 -0300

    fix getCargasHorarias relacionado al legajo

--- Memo1.Recursos.Application/Dtos/CargaHorarias/CargaHorariaDto.cs
+++ Memo1.Recursos.Application/Dtos/CargaHorarias/CargaHorariaDto.cs
@@ -3,7 +3,7 @@ namespace Memo1.Recursos.Application.Dtos.CargaHorarias;
 public class CargaHorariaDto
 {
     public string Id { get; set; }
-    public string Legajo { get; set; }
+    public int Legajo { get; set; }
     public string ProyectoId { get; set; }
     public string TareaId { get; set; }
     public DateOnly Fecha { get; set; }
